de5f6fee999381c672d938ff2ae4b087bdcc8e33
Daniel Hoang <thesubway@gmail.com>
2016-08-15 21:23:07 -0700

    Add rtc3 els

--- finder.rb
+++ finder.rb
@@ -31,3 +31,5 @@ puts "doubles ci3, adv writing #{find_doubles(@ci3, @advanced_writing)}"
 puts "doubles arts, adv writing #{find_doubles(@arts, @advanced_writing)}"
 puts "doubles ethics, adv writing #{find_doubles(@ethics, @advanced_writing)}"
 puts "doubles diversity, adv writing #{find_doubles(@diversity, @advanced_writing)}"
+
+puts "doubles rtc3, els #{find_doubles(@rtc3, @els)}"
